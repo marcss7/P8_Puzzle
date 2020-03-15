271234c5e96460c7c502fc8cee019f6128a585eb
Teresa Garcia Ceva <teregc@hotmail.es>
2020-03-15 10:45:36 +0100

    Initial commit

---
+++ app/src/main/java/edu/uoc/resolvers/Block.java
@@ -0,0 +1,14 @@
+package edu.uoc.resolvers;
+
+public class Block {
+
+    public Block(int position, int vPosition, int hPosition){
+        this.position = position;
+        this.vPosition = vPosition;
+        this.hPosition = hPosition;
+    }
+
+    public int position;
+    public int vPosition;
+    public int hPosition;
+}
